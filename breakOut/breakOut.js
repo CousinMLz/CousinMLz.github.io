4567ce1cf36730600a18bbfb2cb8b3003bdaffc7
CousinMLz <60953926+CousinMLz@users.noreply.github.com>
2023-02-14 04:26:00 +0000

    Add files via upload

--- breakOut/breakOut.js
+++ breakOut/breakOut.js
@@ -1,18 +1,64 @@
+// Drawing Attributes
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 const interval = setInterval(draw, 10);
+
+// Ball Attributes
 const ballRadius = 10;
 let ballColor = "blue";
+
+// Paddle Attributes
 const paddleHeight = 10;
 const paddleWidth = 75;
 let rightPressed = false;
 let leftPressed = false;
 let paddleX = (canvas.width - paddleWidth) / 2;
-let x = canvas.width / 2;
+
+// Brick Attributes
+const brickRowCount = 3;
+const brickColumnCount = 5;
+const brickWidth = 75;
+const brickHeight = 20;
+const brickPadding = 10;
+const brickOffsetTop = 30;
+const brickOffsetLeft = 30;
+
+// Build Brick Pattern
+const bricks = [];
+for (let c = 0; c < brickColumnCount; c++) {
+    bricks[c] = [];
+    for (let r = 0; r < brickRowCount; r++) {
+        bricks[c][r] = { x: 0, y: 0, status: 1 };
+    }
+}
+
+function collisionDetection() {
+    for (let c = 0; c < brickColumnCount; c++) {
+        for (let r = 0; r < brickRowCount; r++) {
+            const b = bricks[c][r];
+            if (b.status === 1) {
+                if (
+                    x > b.x &&
+                    x < b.x + brickWidth &&
+                    y > b.y &&
+                    y < b.y + brickHeight
+                ) {
+                    dy = -dy;
+                    b.status = 0;
+                    ballColor = getColorCode();
+                }
+            }
+        }
+    }
+}
+
+// Movement Attributes
+let x = Math.floor(Math.random() * canvas.width);
 let y = canvas.height - 30;
-let dx = 2;
+let dx = (Math.random() * 2);
 let dy = -2;
 
+// Random Color Generator
 function getColorCode() {
     let makeColorCode = '0123456789ABCDEF';
     let code = '#';
@@ -30,6 +76,26 @@ function drawBall() {
     ctx.closePath();
 }
 
+function drawBricks() {
+    for (let c = 0; c < brickColumnCount; c++) {
+        for (let r = 0; r < brickRowCount; r++) {
+            if (bricks[c][r].status === 1) {
+                const brickX = c * (brickWidth + brickPadding) + brickOffsetLeft;
+                const brickY = r * (brickHeight + brickPadding) + brickOffsetTop;
+                bricks[c][r].x = brickX;
+                bricks[c][r].y = brickY;
+                ctx.beginPath();
+                ctx.rect(brickX, brickY, brickWidth, brickHeight);
+                ctx.fillStyle = "#0095DD";
+                ctx.fill();
+                ctx.closePath();
+            }
+        }
+    }
+}
+
+
+
 function drawPaddle() {
     ctx.beginPath();
     ctx.rect(paddleX, (canvas.height - paddleHeight), paddleWidth, paddleHeight);
@@ -40,12 +106,15 @@ function drawPaddle() {
 
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBricks();
     drawBall();
     drawPaddle();
+    collisionDetection();
+
     x += dx;
     y += dy;
     if (((x + dx) < ballRadius) || ((x + dx) > (canvas.width - ballRadius))) {
-        ballColor = getColorCode();
+        // ballColor = getColorCode();
         dx = -dx;
     }
     if (y + dy < ballRadius) {
@@ -53,7 +122,7 @@ function draw() {
     } else if (y + dy > canvas.height - ballRadius) {
         if (x > paddleX && x < paddleX + paddleWidth) {
             dy = -dy;
-            if((-dy) < 6){
+            if ((-dy) < 8) {
                 --dy;
             }
         } else {
@@ -68,7 +137,7 @@ function draw() {
         paddleX -= 7;
     }
 
-    /*  -------Tutorial Solution to paddle movement-------
+    /*  --Tutorial Solution to paddle movement--
         if (rightPressed) {
             paddleX = Math.min(paddleX + 7, canvas.width - paddleWidth);
         } else if (leftPressed) {
@@ -78,6 +147,7 @@ function draw() {
 
 }
 
+/*--Player Controls--*/
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
 function keyDownHandler(e) {
@@ -95,3 +165,4 @@ function keyUpHandler(e) {
         leftPressed = false;
     }
 }
+/*--End of Controls--*/
