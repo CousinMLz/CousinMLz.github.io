a38bae9fde70bb2ed07bcf83e80d19089cf84df0
cousinmlz <jmillere@outlook.com>
2023-02-23 17:35:09 -0800

    Modifying website to look more appealing. Legacy files are still archived.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,14 +1,3 @@
-let myImage = document.querySelector("img");
-
-myImage.onclick = () => {
-    let mySrc = myImage.getAttribute("src");
-    if (mySrc === "../imgs/fox-sleep.jpg") {
-        myImage.setAttribute("src", "../imgs/fox-awake.jpg");
-    } else {
-        myImage.setAttribute("src", "../imgs/fox-sleep.jpg");
-    }
-};
-
 let myButton = document.querySelector("button");
 let myHeading = document.querySelector("h1");
 
@@ -18,7 +7,7 @@ function setUserName() {
       setUserName();
     } else {
       localStorage.setItem("name", myName);
-      myHeading.textContent = `Don't wake the Fox, ${myName}`;
+      myHeading.textContent = `Try the games out, ${myName}!`;
     }
   }
   
